56ac5b89fbb0a178ee9e0357efbdad7800df92ae
spike <morndragon@126.com>
2016-02-14 19:56:25 +0800

    first commit

---
+++ app/src/main/kotlin/me.chunyu.spike/wcl_kotlin_demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.chunyu.spike.wcl_kotlin_demo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main_tv_message.text = "Hello Kotlin"
+    }
+}
