0a44a07c2af9f958f22f4f1f0c4982af3570f478
spike <morndragon@126.com>
2016-02-15 16:27:30 +0800

    完成.

--- app/src/main/kotlin/me.chunyu.spike/wcl_kotlin_demo/MainActivity.kt
+++ app/src/main/kotlin/me.chunyu.spike/wcl_kotlin_demo/MainActivity.kt
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Kotlin的主类, 添加设置属性.
+ */
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         main_tv_message.text = "Hello Kotlin"
+        main_tv_message.textSize = 20.0f
     }
 }
